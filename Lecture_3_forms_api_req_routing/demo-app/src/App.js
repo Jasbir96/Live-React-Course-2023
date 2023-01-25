15e57bcf8ce624044a2cc79fda0066b0315b1f37
Jasbir <mr.jasbirsingh96@gmail.com>
2023-01-25 22:13:01 +0530

    added api request with react

--- Lecture_3_forms_api_req_routing/demo-app/src/App.js
+++ Lecture_3_forms_api_req_routing/demo-app/src/App.js
@@ -1,8 +1,9 @@
-import logo from './logo.svg';
+
 import './App.css';
-import InputBox from './Components/InputBox';
-import OutputBox from './Components/OutputBox';
-import React, { useState } from 'react'
+import InputBox from './Components/todo/InputBox';
+import OutputBox from './Components/todo/OutputBox';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 function App() {
   let [items, setItems] = useState([]);
   const acceptNewTask = (task) => {
@@ -19,6 +20,36 @@ function App() {
       ></OutputBox>
     </div>
   );
+  console.log("Before");
+  /*        **************************/
+  // let [users, setUsers] = useState([]);
+
+  // useEffect(function () {
+  //   // api request 
+  //   (async function () {
+  //     let res = await axios
+  //       .get("https://jsonplaceholder.typicode.com/users")
+  //     setUsers(res.data);
+  //   })()
+  // }, []);
+  // console.log("I will execute ");
+  // return (
+  //   <>
+  //     <h1>Fetching Data </h1>
+  //     {/*conditional rendering*/}
+  //     {users.length == 0 ? <h2>...Loading</h2> :
+  //       <><h2> Users</h2>
+  //         <ul>
+  //           {users.map((user) => {
+  //             return <li key={user.id}>
+  //               {user.name}</li>
+  //           })}
+  //         </ul>
+  //       </>
+  //     }
+  //   </>
+
+  // )
 }
 
 export default App;
---
+++ factory.md
@@ -0,0 +1,3 @@
+* you can send data to your children using props send as values
+* you can get data from your children  to parent using props send as functions
+* a component only rerenders when it's state is update
